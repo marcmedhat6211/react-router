75bce759e141507291bf598d65387bc531f14380
Marc Medhat <marcmedhat6211@gmail.com>
2022-05-23 15:53:24 +0200

    how to use basic routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route } from "react-router-dom"; // this is a component that allows us to define a certain path and the react component that should be rendered when this path is active in the url
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path={"/welcome"}>
+        <Welcome />
+      </Route>
+      <Route path={"/products"}>
+        <Products />
+      </Route>
     </div>
   );
 }
