a451b6565a958b1fad6e7d0eac7ffe256ed6fc53
Marc Medhat <marcmedhat6211@gmail.com>
2022-05-23 16:44:26 +0200

    using the nav link to add active classes on the active navigation link

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom"; // this link is for basic navigation
+import { NavLink } from "react-router-dom"; // has an extra feature for if you want to add active class on the active navigation link
+
+import classes from "./MainHeader.module.css";
 
 const MainHeader = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <ul>
         <li>
-          <Link to={"/welcome"}>Welcome</Link>
+          <NavLink activeClassName={classes.active} to={"/welcome"}>
+            Welcome
+          </NavLink>
         </li>
         <li>
-          <Link to={"/products"}>Products</Link>
+          <NavLink activeClassName={classes.active} to={"/products"}>
+            Products
+          </NavLink>
         </li>
       </ul>
     </header>
